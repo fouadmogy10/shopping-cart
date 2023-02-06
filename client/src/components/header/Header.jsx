3ff7ba4c81fa921e3bba0e6cb24bc53b498ad201
fouadmogy10 <fouadmogy105@gmail.com>
2023-02-06 19:29:25 +0200

    header comp

---
+++ client/src/components/header/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "../../css/header/Header.css"
+const Header =()=>{
+    return(
+        <header className="text-center bg-secondary text-white py-2">
+        <h2 className="fw-bolder ">Shopping cart</h2>
+      </header>
+    )
+}
+
+export default Header;
