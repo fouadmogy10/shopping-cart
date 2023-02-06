3756c1a213d90d8add3b1544e32e2aea5a352e9c
fouadmogy10 <fouadmogy105@gmail.com>
2023-02-06 20:15:44 +0200

    add data json

--- client/src/components/footer/Footer.jsx
+++ client/src/components/footer/Footer.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import "../../css/footer/footer.css"
+import {words} from "../../words"
 const Footer =()=>{
     return(
         <footer>
         <div>
           <footer className="bg-dark text-center text-lg-start">
             <div className="text-center text-light p-3">
-              © 2020 Copyright :
+              {words.footerTitel}
               <span className="text-muted fw-bolder"> fouad FMF</span>
             </div>
           </footer>
---
+++ client/src/words.js
@@ -0,0 +1,6 @@
+export const words = {
+    headerTitle:"shopping cart",
+    footerTitel:" © 2020 Copyright :",
+    content : "Content",
+    addToCart: "Add To Cart"
+}
