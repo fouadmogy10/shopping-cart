62d458677be9fc38f7fde5e3471b2980394e4557
fouadmogy10 <fouadmogy105@gmail.com>
2023-02-06 21:49:59 +0200

    products-comp

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,28 @@
 import "./App.css";
 import Footer from "./components/footer/Footer";
 import Header from "./components/header/Header";
+import { Col, Container, Row } from "react-bootstrap";
+import Products from "./components/products/Products";
 
 function App() {
   return (
     <div className="layout">
-     <Header/>
-      <main>content</main>
-     <Footer/>
+      <Header />
+      <main>
+        <Container>
+          <Row className="py-5">
+            <Col md="8" sm="7">
+              <Products/>
+            </Col>
+            <Col md="4" sm="5">
+              filter
+            </Col>
+          </Row>
+        </Container>
+      </main>
+      <Footer />
     </div>
   );
 }
-
 
 export default App;
