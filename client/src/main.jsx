2ff8d02450fbc6e62f4c6a7bdbff6126c21479b5
fouadmogy10 <fouadmogy105@gmail.com>
2023-02-10 03:29:41 +0200

    REDUX STORE

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,9 +4,13 @@ import App from './App'
 import './index.css'
 import "bootstrap/dist/js/bootstrap.bundle"
 import "bootstrap/dist/css/bootstrap.min.css"
-
+import store from './store/store'
+import { Provider } from 'react-redux'
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <>
+    <Provider store={store}>
     <App />
-  </React.StrictMode>,
+    </Provider>
+  </>
+  ,
 )
